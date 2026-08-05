ef80b2efddc425c2dd9e567e47dbcc4dd76bcb99
Kevin Oberlies <favilo@gmail.com>
2026-08-05 12:12:42 -0700

    chore(subagent): add extension entrypoint
    Signed-off-by: Kevin Oberlies <favilo@gmail.com>

---
+++ extensions/subagent/index.ts
@@ -0,0 +1,5 @@
+import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
+
+export default function subagentExtension(_pi: ExtensionAPI): void {
+  // Registration is introduced by the first production tracer slice.
+}
